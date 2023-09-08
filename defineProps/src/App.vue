89756b2a52ce3ae76fe55ac2ed63f71b1cc08fdb
easydevgit <urserg1@mail.com>
2023-09-08 20:34:43 +0400

    defineProps examples

---
+++ defineProps/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import ChildComponent from './components/ChildComponent.vue'
+const count = ref(1)
+
+</script>
+
+<template>
+  <ChildComponent :count="count" />
+  <button @click="count++">Increase Count</button>
+</template>
+
+
---
+++ defineProps/src/components/ChildComponent.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { watchEffect } from 'vue'
+
+// runtime declaration
+// const props = defineProps({
+//   msg: {
+//     type: String,
+//     required: false,
+//     default: 'Hello'
+//   }
+// })
+
+export interface Props {
+  count?: number
+}
+
+// type declaration
+const { count } = defineProps<Props>()
+
+watchEffect(() => {
+  console.log(count)
+})
+
+</script>
+
+<template>
+  <h1>{{ count }}</h1>
+</template>
