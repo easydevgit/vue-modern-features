5087c731a03028bce4ddfd85d74b5d010a0dd795
easydevgit <urserg1@mail.com>
2023-09-24 12:57:03 +0400

    defineModel

---
+++ defineModel/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import ChildComponent from './components/ChildComponent.vue'
+import { ref } from 'vue'
+
+const greeting = ref('')
+const farewell = ref('')
+
+</script>
+
+<template>
+  <ChildComponent v-model:greeting="greeting" v-model:farewell="farewell" />
+
+  <div>
+    Greeting: {{ greeting }}
+  </div>
+
+  <div>
+    Farewell: {{ farewell }}
+  </div>
+</template>
---
+++ defineModel/src/components/ChildComponent.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const greeting = defineModel<string>('greeting')
+const farewell = defineModel<string>('farewell')
+
+</script>
+
+<template>
+    <input v-model="greeting" />
+    <input v-model="farewell" />
+</template>
