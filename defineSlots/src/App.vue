71cd4d2a79fe142844d552cd864afe2e62b46429
easydevgit <urserg1@mail.com>
2023-10-29 17:23:48 +0400

    defineSlots

---
+++ defineSlots/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import DaisyUIModal from './components/DaisyUIModal.vue'
+</script>
+
+<template>
+  <DaisyUIModal>
+    <div>
+      default slot
+    </div>
+    <template #body="{ short }">
+      <p>Modal body first paragraph</p>
+      <p v-if="!short">Modal body second paragraph</p>
+    </template>
+  </DaisyUIModal>
+</template>
---
+++ defineSlots/src/components/DaisyUIModal.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const slots = defineSlots<{
+    title?: (props: { msg?: string }) => any
+    body?: (props: { short: boolean }) => any
+    default?: (props: {}) => any;
+}>()
+</script>
+
+<template>
+    <dialog id="my_modal_1" class="modal" open>
+        <div class="modal-box">
+            <h3 class="font-bold text-lg">
+                <slot name="title" />
+                <p v-if="!slots.title">Some title</p>
+            </h3>
+            <slot name="body" :short="false" />
+            <slot></slot>
+            <div class="modal-action">
+                <form method="dialog">
+                    <!-- if there is a button in form, it will close the modal -->
+                    <button class="btn">Close</button>
+                </form>
+            </div>
+        </div>
+    </dialog>
+</template>
